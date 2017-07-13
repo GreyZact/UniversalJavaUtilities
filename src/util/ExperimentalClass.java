42d4938fe703e2f900e4db998fda99699efa3081
Mike <michal@madalinski.in>
2017-07-13 11:39:26 +0200

    add doc

--- src/util/ExperimentalClass.java
+++ src/util/ExperimentalClass.java
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author mike
  */
-public class NewClass {
+public class ExperimentalClass {
     
     public static void main(String[] args) {
         Debug debug = new Debug("NewClass> ");
